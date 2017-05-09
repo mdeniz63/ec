627355a097097016c48708acb5e4d6ab13c5a4cd
mdeniz63 <mdeniz63@gmail.com>
2017-05-09 20:30:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
+echo "Run as root user on Centos 7.3"
 
-echo "Run as root user"
-
+# Install docker ce with Yum
 yum install -y yum-utils
-
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
 yum makecache fast
-
 yum install -y docker-ce
 
+# Start and enable docker
 systemctl start docker
 systemctl enable docker
 
+# Create docker volumes
 docker volume create files
 docker volume create nitro
 
-
+# Pull docker images from docker hub
 image_name=mdeniz63/ec
 container_name=ec
-
 docker pull $image_name
 
+# Run image and create container with systemd services
 docker run --privileged --name $container_name -v files:/ARSIV -v nitro:/nitro/ -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:8181 -p 5432:5432 -d $image_name
 
-# Systemd service
+# Add Systemd service and enable
 cat > /usr/lib/systemd/system/ec.service << EOF1
 
 [Unit]
