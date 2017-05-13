0d13e09ad3a2ec094b12a3114e2d1c512d61bc12
mdeniz63 <mdeniz63@gmail.com>
2017-05-13 22:05:43 +0300

    Update schedule_postgresql_backup.sh

--- schedule_postgresql_backup.sh
+++ schedule_postgresql_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-line="00 00 * * * /bin/sh /nitro/script/backup.sh > /nitro/backups/backup.log 2>&1"
+line="00 00 * * * /bin/sh /nitro/script/backup_postgresql.sh > /nitro/backups/backup_postgresql.log 2>&1"
 (crontab -u postgres -l; echo "$line" ) | crontab -u postgres -
