21084aaa03481f32fadd8dfac34efb4e9b8cee34
mdeniz63 <mdeniz63@gmail.com>
2017-05-19 00:32:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ yum install -y docker-ce
 
 # Add docker config file /etc/docker/deamon.json
 mkdir -p /home/ec_data
+cd /etc/docker/
 curl https://raw.githubusercontent.com/mdeniz63/ec/master/daemon.json > daemon.json
 
 # Start and enable docker
