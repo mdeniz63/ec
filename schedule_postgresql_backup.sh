33092e565629a8eff13a02324d2966fc4d5d30c2
mdeniz63 <mdeniz63@gmail.com>
2017-12-11 15:37:06 +0300

    Delete schedule_postgresql_backup.sh

--- schedule_postgresql_backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-line="00 00 * * * /bin/sh /nitro/script/backup_postgresql.sh > /nitro/backups/backup_postgresql.log 2>&1"
-(crontab -u postgres -l; echo "$line" ) | crontab -u postgres -
